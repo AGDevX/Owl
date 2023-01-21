1db9c7d1b66195c173e7a74dc4c90dec6392edcf
August F. Geier V <AGDevX@gmail.com>
2023-01-21 15:34:00 -0500

    Use Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Outlet } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './state/redux/store';
 import AuthNProvider from './auth/authN/AuthNProvider';
+import Layout from './structure/Layout';
 
 const App = () => {
 	return (
 		<Provider store={store}>
 			<AuthNProvider>
-				<Outlet />
+				<Layout />
 			</AuthNProvider>
 		</Provider>
 	);
