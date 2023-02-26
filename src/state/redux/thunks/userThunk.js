aa85782881775b672692ef6ddfe628a42613afa4
August F. Geier V <AGDevX@gmail.com>
2023-02-26 10:28:58 -0500

    Removed reduxHistoryThunk example code

--- src/state/redux/thunks/userThunk.js
+++ src/state/redux/thunks/userThunk.js
@@ -1,12 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { navigateToErrorRoute } from './reduxHistoryThunk';
 import { getUserInfo } from '../../../apis/spider-api';
 
-export const getInfo = createAsyncThunk('user-info/get', async (email, { dispatch, rejectWithValue }) => {
+export const getInfo = createAsyncThunk('user-info/get', async (email, { rejectWithValue }) => {
 	const response = await getUserInfo(email);
 
 	if (response.statusCode === 500) {
-		dispatch(navigateToErrorRoute());
 		return rejectWithValue(response);
 	}
 
