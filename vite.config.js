2aaf39e84fe739e7b91689802057197f6d1a4cc0
August F. Geier V <AGDevX@gmail.com>
2023-10-09 20:52:33 -0400

    remove assetsDir from vite.config.js (unnecessary)

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
 	},
 	build: {
 		outDir: '../dist',
-		assetsDir: '',
 		rollupOptions: {
 			output: {
 				entryFileNames: `app-${env.ENVIRONMENT.app}-[name]-[hash].js`,
